389b4e8e954d19461d049d12a4d32315e134710b
Andrew Hammond <andrew@evertrue.com>
2013-01-07 17:23:19 -0500

    updated setuplinks

--- setuplinks.sh
+++ setuplinks.sh
@@ -1,5 +1,7 @@
 #!/bin/zsh
-
 ln -s ~/setupfiles/.zshrc ~/.zshrc
+ln -s ~/setupfiles/.bash_profile ~/.bash_profile
 ln -s ~/setupfiles/.zprofile ~/.zprofile
 ln -s ~/setupfiles/.gitconfig ~/.gitconfig
+ln -s ~/setupfiles/.pearrc ~/.pearrc
+ln -s ~/setupfiles/php.ini ~/etc/php.ini
