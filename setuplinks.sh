bf504e442105e5f7c30afa8c08593ff6d1844a63
Andrew Hammond <andrew@evertrue.com>
2013-01-07 17:33:48 -0500

    added apache conf

--- setuplinks.sh
+++ setuplinks.sh
@@ -5,3 +5,4 @@ ln -s ~/setupfiles/.zprofile ~/.zprofile
 ln -s ~/setupfiles/.gitconfig ~/.gitconfig
 ln -s ~/setupfiles/.pearrc ~/.pearrc
 ln -s ~/setupfiles/php.ini ~/etc/php.ini
+ln -s ~/setupfiles/httpd.conf ~/etc/apache2/httpd.conf
