92f0309b1d5acc281fdcbdea9c2b9b5de7678394
Andrew Hammond <andrew@evertrue.com>
2013-01-07 17:41:31 -0500

    fixed setuplinks

--- setuplinks.sh
+++ setuplinks.sh
@@ -1,8 +1,8 @@
 #!/bin/zsh
-ln -s ~/setupfiles/.zshrc ~/.zshrc
-ln -s ~/setupfiles/.bash_profile ~/.bash_profile
-ln -s ~/setupfiles/.zprofile ~/.zprofile
-ln -s ~/setupfiles/.gitconfig ~/.gitconfig
-ln -s ~/setupfiles/.pearrc ~/.pearrc
-ln -s ~/setupfiles/php.ini ~/etc/php.ini
-ln -s ~/setupfiles/httpd.conf ~/etc/apache2/httpd.conf
+ln -s ~/EverTrue/dev/setupfiles/.zshrc ~/.zshrc
+ln -s ~/EverTrue/dev/setupfiles/.bash_profile ~/.bash_profile
+ln -s ~/EverTrue/dev/setupfiles/.zprofile ~/.zprofile
+ln -s ~/EverTrue/dev/setupfiles/.gitconfig ~/.gitconfig
+ln -s ~/EverTrue/dev/setupfiles/.pearrc ~/.pearrc
+ln -s ~/EverTrue/dev/setupfiles/php.ini /etc/php.ini
+ln -s ~/EverTrue/dev/setupfiles/httpd.conf /etc/apache2/httpd.conf
